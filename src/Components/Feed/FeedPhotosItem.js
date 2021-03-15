4010ed307cba63887bcfc8c9f93e26c7af404eab
Paulo Victor <pvcunha05@gmail.com>
2021-03-15 19:03:11 -0300

    Feed Modal Component

--- src/Components/Feed/FeedPhotosItem.js
+++ src/Components/Feed/FeedPhotosItem.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from '../../Styles/Feed/FeedPhotosItem.module.css';
 
-const FeedPhotosItem = ({ photo }) => {
+const FeedPhotosItem = ({ photo, setModalPhoto }) => {
   return (
-    <li className={styles.photo}>
+    <li className={styles.photo} onClick={() => setModalPhoto(photo)}>
       <img src={photo.src} alt={photo.title} />
       <span>{photo.acessos}</span>
     </li>
