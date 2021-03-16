c601e28198ef2747a2ed2369d9e46fa516b59d2b
Paulo Victor <pvcunha05@gmail.com>
2021-03-16 12:43:38 -0300

    Image Skeleton Component

--- src/Components/Feed/FeedPhotosItem.js
+++ src/Components/Feed/FeedPhotosItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from '../../Styles/Feed/FeedPhotosItem.module.css';
+import Image from '../Helper/Image';
 
 const FeedPhotosItem = ({ photo, setModalPhoto }) => {
   return (
     <li className={styles.photo} onClick={() => setModalPhoto(photo)}>
-      <img src={photo.src} alt={photo.title} />
+      <Image src={photo.src} alt={photo.title} />
       <span>{photo.acessos}</span>
     </li>
   );
