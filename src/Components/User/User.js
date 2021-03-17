2909ca754d68be846c2953b845b7af64b835eeca
Paulo Victor <pvcunha05@gmail.com>
2021-03-17 16:04:53 -0300

    Loading & Infinite Scroll Components

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
+import UserContext from '../../Context/UserContext';
 import Feed from '../Feed/Feed';
 import UserHeader from './UserHeader';
 import UserPhotoPost from './UserPhotoPost';
 import UserStats from './UserStats';
 
 const User = () => {
+  const userContext = React.useContext(UserContext);
+  const { user } = userContext;
+
   return (
     <section className="container">
       <UserHeader />
 
       <Routes>
-        <Route path="/" element={<Feed />} />
+        <Route path="/" element={<Feed user={user.id} />} />
         <Route path="/postar" element={<UserPhotoPost />} />
         <Route path="/estatisticas" element={<UserStats />} />
       </Routes>
