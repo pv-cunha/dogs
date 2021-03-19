c8612aa80172bec1bddff6e85016bf382c3c5a90
Paulo Victor <pvcunha05@gmail.com>
2021-03-19 11:24:11 -0300

    Victory Lib

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import Head from '../Helper/Head';
+import Error from '../Helper/Error';
+import Loading from '../Helper/Loading';
+import useFetch from '../../Hooks/useFetch';
+import { GET_STATS } from '../../api';
+const UserStatsGraphs = React.lazy(() => import('./UserStatsGraphs'));
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-      Estatísticas
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    const getData = async () => {
+      const token = window.localStorage.getItem('token');
+
+      const { url, options } = GET_STATS(token);
+
+      await request(url, options);
+    };
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <React.Suspense fallback={<div></div>}>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </React.Suspense>
+    );
+  else return null;
 };
 
 export default UserStats;
